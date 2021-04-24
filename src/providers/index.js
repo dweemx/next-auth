5651900b73e1193343ad6e1da266134f3b8015e7
dweemx <mdewaegeneer@gmail.com>
2021-04-24 17:32:30 +0200

    feat: build

---
+++ src/providers/index.js
@@ -0,0 +1,83 @@
+import Apple from "./apple.js"
+import Atlassian from "./atlassian.js"
+import Auth0 from "./auth0.js"
+import AzureADB2C from "./azure-ad-b2c.js"
+import Basecamp from "./basecamp.js"
+import BattleNet from "./battlenet.js"
+import Box from "./box.js"
+import Bungie from "./bungie.js"
+import Cognito from "./cognito.js"
+import Credentials from "./credentials.js"
+import Discord from "./discord.js"
+import Email from "./email.js"
+import EVEOnline from "./eveonline.js"
+import Facebook from "./facebook.js"
+import FACEIT from "./faceit.js"
+import Foursquare from "./foursquare.js"
+import FusionAuth from "./fusionauth.js"
+import GitHub from "./github.js"
+import GitLab from "./gitlab.js"
+import Google from "./google.js"
+import IdentityServer4 from "./identity-server4.js"
+import Instagram from "./instagram.js"
+import Kakao from "./kakao.js"
+import LINE from "./line.js"
+import LinkedIn from "./linkedin.js"
+import MailRu from "./mailru.js"
+import Medium from "./medium.js"
+import Netlify from "./netlify.js"
+import Okta from "./okta.js"
+import Osso from "./osso.js"
+import Reddit from "./reddit.js"
+import Salesforce from "./salesforce.js"
+import Slack from "./slack.js"
+import Spotify from "./spotify.js"
+import Strava from "./strava.js"
+import Twitch from "./twitch.js"
+import Twitter from "./twitter.js"
+import VK from "./vk.js"
+import Yandex from "./yandex.js"
+import Zoho from "./zoho.js"
+
+export default {
+  Apple,
+  Atlassian,
+  Auth0,
+  AzureADB2C,
+  Basecamp,
+  BattleNet,
+  Box,
+  Bungie,
+  Cognito,
+  Credentials,
+  Discord,
+  Email,
+  EVEOnline,
+  Facebook,
+  FACEIT,
+  Foursquare,
+  FusionAuth,
+  GitHub,
+  GitLab,
+  Google,
+  IdentityServer4,
+  Instagram,
+  Kakao,
+  LINE,
+  LinkedIn,
+  MailRu,
+  Medium,
+  Netlify,
+  Okta,
+  Osso,
+  Reddit,
+  Salesforce,
+  Slack,
+  Spotify,
+  Strava,
+  Twitch,
+  Twitter,
+  VK,
+  Yandex,
+  Zoho,
+}
